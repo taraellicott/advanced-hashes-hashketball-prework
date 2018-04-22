488b05e9c707b4668339c5c0e304c56f89aa0e9c
Tara Ellicott <taraellicott@gmail.com>
2018-04-22 02:06:09 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -156,6 +156,12 @@ def teams
   game_hash.values
 end
 
+
+def find_the_team(team_name)
+  teams.find {|team| team.fetch(:team_name) == team_name}
+end
+
+
 # def team_colors(team_name)
 #   # returns array of teams colors
 # end
